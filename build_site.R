5b0606e773743c027bea24facc9d459777830f2f
Peter EerNisse <peernisse@stantec.com>
2020-12-31 13:48:04 -0700

    Added more project examples. Made link to Shiny app open in new window.

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:/R_Projects/resume_web")
+setwd("C:/R_Projects/portfolio")
 
 #render your sweet site. 
 rmarkdown::render_site()
